6f94681be66f7b3e909937dedb34bca2838db43c
Edric Jay M. Rivas <rivas-kdt@kdts.net>
2024-11-14 12:10:56 +0800

    try commit23

--- qrcodegen/api/getData.js
+++ qrcodegen/api/getData.js
@@ -1,13 +1,12 @@
-import { db } from "@vercel/postgres";
-
+import { sql } from '@vercel/postgres';
+ 
 export default async function handler(request, response) {
-  const client = await db.connect();
   try {
-    await client.sql`INSERT INTO qrdata (id, url, latitude, longitude) VALUES (1,'2',3,4);`;
+    await sql`INSERT INTO qrdata (id, url, latitude, longitude) VALUES (1,'2',3,4);`;
   } catch (error) {
     return response.status(500).json({ error });
   }
-
-  const qrdata = await sql`SELECT * FROM qrdata`;
-  return response.status(200).json({ qrdata });
+ 
+  const pets = await sql`SELECT * FROM qrdata`;
+  return response.status(200).json({ pets });
 }

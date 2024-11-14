952579ee5f82c59154ba03ee34c727855a0fe7a5
Edric Jay M. Rivas <rivas-kdt@kdts.net>
2024-11-14 13:02:02 +0800

    try commit23

--- qrcodegen/api/hello.js
+++ qrcodegen/api/hello.js
@@ -1,10 +1,8 @@
-import { sql } from '@vercel/postgres';
-
 export default function handler(req, res) {
     // Send a response with status 200 and a JSON object
     let pet
-    const fetchData = async () => {
-      const pets = await sql`SELECT * FROM Pets;`;
+    const fetchData = () => {
+      const pets = `SELECT * FROM Pets;`;
       pet = pets
     }
     fetchData()

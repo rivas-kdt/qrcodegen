2a4dc7ed0e34a3a546a91295e4c8e0eedeecd6d8
Edric Jay M. Rivas <rivas-kdt@kdts.net>
2024-11-14 13:04:49 +0800

    try commit23

--- qrcodegen/api/hello.js
+++ qrcodegen/api/hello.js
@@ -1,11 +1,6 @@
-export default function handler(req, res) {
+export default async function handler(req, res) {
     // Send a response with status 200 and a JSON object
-    let pet
-    const fetchData = () => {
-      const pets = `SELECT * FROM Pets;`;
-      pet = pets
-    }
-    fetchData()
-    res.status(200).json({ pet });
+      const pets = await `SELECT * FROM Pets;`;
+    res.status(200).json({ pets });
   }
   
